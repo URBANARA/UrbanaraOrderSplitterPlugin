ed4b0b394cbbbafb6e5c9e1bbc2762586ce76ff7
Manuel Gomez <manuel.gomez@urbanara.com>
2017-08-02 15:41:57 +0200

    ITK-598_splitting_order_rules -moved to external rules -temp commit

--- src/DependencyInjection/OrderSplitterExtension.php
+++ src/DependencyInjection/OrderSplitterExtension.php
@@ -5,18 +5,10 @@ namespace Urbanara\OrderSplitterPlugin\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 final class OrderSplitterExtension extends Extension
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function load(array $config, ContainerBuilder $container)
-    {
-        $config = $this->processConfiguration($this->getConfiguration([], $container), $config);
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-    }
 
     /**
      * {@inheritdoc}
@@ -24,5 +16,18 @@ final class OrderSplitterExtension extends Extension
     public function getAlias()
     {
         return 'order_splitter';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('state_machine.yml');
+        $loader->load('services.yml');
     }
 }
